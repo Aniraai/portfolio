74b5e37e9ebecf25cb20230edcc4731a2390ec70
Ankit rai <122200322+Aniraai@users.noreply.github.com>
2024-03-10 11:24:10 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+const App = () => {
+  return;
+  <div>
+    <section>Hero</section>
+    <section>Parallex</section>
+    <section>Services</section>
+    <section>Parallex</section>
+    <section>Portfolio1</section>
+    <section>Portfolio2</section>
+    <section>Portfolio3</section>
+    <section>Contact</section>
+  </div>;
+};
+
+export default App;
